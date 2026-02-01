2e615f8119cd2527f0fa8ade8b853f9149ddb27c
shineakhanda4 <shineakhanda49@gmail.com>
2026-02-01 02:41:20 +0000

    Refactor post section to use article element

--- index.php
+++ index.php
@@ -1,15 +1,10 @@
-<?php get_header(); // This pulls in the code above, including wp_head() ?>
+<?php get_header(); ?>
 
 <main>
     <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-        <section id="post-<?php the_ID(); ?>">
-            <div class="logo-container">
-                <h1><?php the_title(); ?></h1>
-            </div>
-            <div class="entry-content">
-                <?php the_content(); ?>
-            </div>
-        </section>
+        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+            <?php the_content(); ?>
+        </article>
     <?php endwhile; endif; ?>
 </main>
 
