c60fa3155371ef0fb2faf88a8b6a67961dd14de5
shineakhanda4 <shineakhanda49@gmail.com>
2026-02-01 02:52:43 +0000

    Simplify post output in index.php

--- index.php
+++ index.php
@@ -2,9 +2,7 @@
 
 <main>
     <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-            <?php the_content(); ?>
-        </article>
+        <?php the_content(); ?>
     <?php endwhile; endif; ?>
 </main>
 
