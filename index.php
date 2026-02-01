963f4cd4f97fa515a2dfc4572c3325a1d2811979
shineakhanda4 <shineakhanda49@gmail.com>
2026-02-01 02:28:55 +0000

    Refactor header and footer function calls
    Added comment to header function call and removed extra newlines.

--- index.php
+++ index.php
@@ -1,20 +1,15 @@
-<?php get_header(); ?>
+<?php get_header(); // This pulls in the code above, including wp_head() ?>
 
 <main>
     <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-        
         <section id="post-<?php the_ID(); ?>">
-            
             <div class="logo-container">
                 <h1><?php the_title(); ?></h1>
             </div>
-
             <div class="entry-content">
                 <?php the_content(); ?>
             </div>
-
         </section>
-
     <?php endwhile; endif; ?>
 </main>
 
