32310846ca48f93d830e5ef3d17150cc007ca513
shineakhanda4 <shineakhanda49@gmail.com>
2026-02-01 02:25:09 +0000

    Add script for home URL in header.php

--- header.php
+++ header.php
@@ -3,7 +3,12 @@
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <?php wp_head(); // Essential for plugins and enqueued styles ?>
+    
+    <script>
+        const wpHomeUrl = "<?php echo esc_url(home_url()); ?>";
+    </script>
+
+    <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
     <header-component></header-component>
